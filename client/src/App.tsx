30f670728c713990e1c6f6f9eaaf280fc2fe677e
Pavel <pavelparshin2002005@gmail.com>
2025-03-22 19:27:20 +0300

    done

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { BlackSquare } from "./components/BlackSquare";
+
+function App() {
+  return (
+    <div className="app">
+      <BlackSquare />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BlackSquare/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./BlackSquare.module.css";
+
+export const BlackSquare = () => {
+  return <div className={styles.square}></div>;
+};
